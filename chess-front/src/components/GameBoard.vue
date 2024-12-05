b1a4d315614a466bc6d175ec844895e52542e6cd
Maxime <maxime.monceau@etu.univ-lyon1.fr>
2024-12-05 15:19:30 +0100

    animation

--- chess-front/src/components/GameBoard.vue
+++ chess-front/src/components/GameBoard.vue
@@ -325,15 +325,15 @@ const quitGame = async () => {
 
 @keyframes pulse {
   0% {
-    box-shadow: inset 0 0 0 4px rgba(0, 128, 255, 0.4);
+    box-shadow: inset 0 0 0 8px rgba(0, 128, 255, 0.4);
   }
 
   50% {
-    box-shadow: inset 0 0 0 4px rgba(0, 128, 255, 0.2);
+    box-shadow: inset 0 0 0 8px rgba(0, 128, 255, 0.2);
   }
 
   100% {
-    box-shadow: inset 0 0 0 4px rgba(0, 128, 255, 0.4);
+    box-shadow: inset 0 0 0 8px rgba(0, 128, 255, 0.4);
   }
 }
 
