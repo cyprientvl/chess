952744473bb5a1c47ee559d897d74612db7d5385
Maxime <maxime.monceau@etu.univ-lyon1.fr>
2024-12-05 08:58:58 +0100

    toast life

--- chess-back/src/controllers/auth.controller.ts
+++ chess-back/src/controllers/auth.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Middlewares, Post, Route, Security, Tags } from "tsoa";
-import { UserDTO } from "../dto/user.dto";
+import { Body, Controller, Post, Route, Tags } from "tsoa";
 import { AuthBody } from "../interfaces/authBody.interface";
 import { authService } from "../services/auth.service";
 
@@ -8,20 +7,20 @@ import { authService } from "../services/auth.service";
 export class AuthController extends Controller {
 
   @Post("/login")
-  public async auth(@Body() bodyAuth: AuthBody): Promise<{token: string} | null>{
+  public async auth(@Body() bodyAuth: AuthBody): Promise<{ token: string } | null> {
     const token = await authService.auth(bodyAuth);
 
-    if(bodyAuth.grant_type != 'password'){
+    if (bodyAuth.grant_type != 'password') {
       let error = new Error("Invalid grant_type");
       (error as any).status = 400;
-      throw error;      
+      throw error;
     }
 
-    if(!token){
-        this.setStatus(404);
-        return null;
+    if (!token) {
+      this.setStatus(404);
+      return null;
     }
 
-    return {token: token};
+    return { token: token };
   }
 }
