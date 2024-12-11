569a248a1d4a301f5fc177d53c15f7be2f83c40e
Maxime <maxime.monceau@etu.univ-lyon1.fr>
2024-12-11 16:47:31 +0100

    replay

--- chess-front/src/model/Replay.model.ts
+++ chess-front/src/model/Replay.model.ts
@@ -1,8 +1,17 @@
-import type { Color, PieceType } from "./Pieces.model";
+import type { Color, Piece, PieceType } from "./Pieces.model";
+
+export interface ChessReplay {
+  ownerUsername: string;
+  ownerColor: Color;
+  actions: ReplayStep[];
+}
 
 export interface ReplayStep {
   i: number;
   j: number;
+  toI: number;
+  toJ: number;
   piece: PieceType | undefined;
   color: Color | undefined;
+  pieceKilled: Piece | undefined;
 }
