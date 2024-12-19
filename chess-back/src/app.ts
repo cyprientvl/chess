bacae4f46783876e9fb0a14f42a05caf26f6afe6
iutbgdin <iutbgdin@iutabgdinvm-68.iutbourg.univ-lyon1.fr>
2024-12-19 15:07:10 +0100

    patch

--- chess-back/src/app.ts
+++ chess-back/src/app.ts
@@ -22,7 +22,7 @@ declare global {
 
 app.use(cors({
   origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
